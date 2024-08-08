b2c79392c2f50ea53ccec0370fdf2544a3fe1dc7
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-08-08 14:25:52 +0530

    SQLite Viewer init

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * Name: SQLite Viewer
+ * Description: SQLite Viewer for Node JS or JavaScript and TypeScript.
+ * Version: 0.0.1
+ * Developer: Sushil Kumar
+ * Github: https://github.com/SQLiteAdmin/SQLite-Viewer.git
+ * */
+
+const path = require("path");
